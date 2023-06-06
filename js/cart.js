9bbb9ac5c67bcef651a8ec0fa08a62480546b973
preetbhingradiya <preetbhingradiya6@gmail.com>
2023-06-06 23:04:14 +0530

    card quntity

--- js/cart.js
+++ js/cart.js
@@ -2,7 +2,7 @@ import nav from "../extra/index.js";
 
 document.querySelector(".navbar").innerHTML = nav();
 
-let cart = JSON.parse(localStorage.getItem("cart"))||[];
+let cart = JSON.parse(localStorage.getItem("cart")) || [];
 
 const show = (data) => {
   let price = 0;
@@ -12,35 +12,56 @@ const show = (data) => {
   console.log(price);
 
   data.map((ele) => {
-    
-    let div=document.createElement("div")
-    div.setAttribute("class","set")
+    let div = document.createElement("div");
+    div.setAttribute("class", "set");
 
-    let img=document.createElement("img")
-    img.src=ele.images[0];
+    let img = document.createElement("img");
+    img.src = ele.images[0];
 
     let titale = document.createElement("h2");
     titale.innerHTML = ele.title;
     let cat = document.createElement("h5");
     cat.innerHTML = ele.category;
-    let temp=document.createElement("div")
-    temp.setAttribute("class","wrapper")
-    temp.append(titale,cat)
+    let temp = document.createElement("div");
+    temp.setAttribute("class", "wrapper");
+    temp.append(titale, cat);
 
-    let incre=document.createElement("button")
-    incre.innerHTML="+"
-    let quntity=document.createElement("p")
-    quntity.innerHTML=1
-    let desc=document.createElement("button")
-    desc.innerHTML="-"
-    let store=document.createElement("div")
-    store.setAttribute("class","qyt")
-    store.append(desc,quntity,incre)
+    let incre = document.createElement("button");
+    incre.innerHTML = "+";
+    let quntity = document.createElement("p");
+    quntity.innerHTML = 1;
+    let desc = document.createElement("button");
+    desc.innerHTML = "-";
+    let store = document.createElement("div");
+    store.setAttribute("class", "qyt");
+    store.append(desc, quntity, incre);
 
+    div.append(img, temp, store);
+    document.getElementById("cart").append(div);
 
+    incre.addEventListener("click", () => {
+      let cart = JSON.parse(localStorage.getItem("cart")) || [];
 
+      cart.map((val, index) => {
+        if (val.id == ele.id) {
+            console.log((cart[index].qty += 1));
+          localStorage.setItem("cart", JSON.stringify(cart));
+        }
+      });
+    });
+
+    desc.addEventListener("click", () => {
+      let cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+      cart.map((val, index) => {
+        if (val.id == ele.id) {
+    
+          console.log( (cart[index].qty -= 1));
+          localStorage.setItem("cart", JSON.stringify(cart));
+        }
+      });
+    });
   });
 };
-
 
 show(cart);
