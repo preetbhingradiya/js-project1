286dea1073cee178409a54cfa333ba1705da993e
preetbhingradiya <preetbhingradiya6@gmail.com>
2023-06-07 15:35:29 +0530

    improve css

--- extra/index.js
+++ extra/index.js
@@ -1,6 +1,6 @@
 const nav = () => {
   return `<ul class="navbar-ul">
-    <a href="index.html"><li>Home</li></a>
+    <a href="../index.html"><li>Home</li></a>
     <a href="../pages/product.html"><li>Products</li></a>
     <a href="../pages/cart.html"><li>Cart</li></a>
     <a href="../pages/login.html"><li>Login</li></a>
